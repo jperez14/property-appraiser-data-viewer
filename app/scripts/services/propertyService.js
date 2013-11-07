8ad33f19b0086f9dd1ecbea70dabe2615d9fc42d
Jorge Fiallega <jorgefiallega@gmail.com>
2013-11-07 17:48:09 -0500

    added ownersInfo, extraFeatures and landLines to the property model.

--- app/scripts/services/propertyService.js
+++ app/scripts/services/propertyService.js
@@ -7,16 +7,16 @@ angular.module('propertySearchApp')
     var Property = function(data) {
 
       this.propertyInfo = {};
-      this.siteAddresses = [];
-      this.salesInfo = [];
-      this.ownersInfo = [];
       this.legalDescription = {};
       this.mailingAddress = {};
       this.assessmentInfo = {};
       this.classifiedAgInfo = {};
       this.exemptionInfo = {};
+      this.siteAddresses = [];
+      this.salesInfo = [];
+      this.ownersInfo = [];
       this.extraFeatures = [];
-      this.landCInfo = [];
+      this.landLines = [];
 
       this.district = null;
       this.geoParcel = null;
@@ -24,21 +24,174 @@ angular.module('propertySearchApp')
       if(isUndefinedOrNull(data)) {
         this.propertyInfo = buildPropertyInfo({});
         this.siteAddresses = buildSiteAddresses([]);
+        this.salesInfo = buildSalesInfo([]);
+        this.ownersInfo = buildOwnersInfo([]);
+        this.extraFeatures = buildExtraFeatures([]);
+        this.landLines = buildLandLines([]);
       }
       else{
         this.propertyInfo = buildPropertyInfo(data.PropertyInfo);
         this.siteAddresses = buildSiteAddresses(data.SiteAddresses);
+        this.salesInfo = buildSalesInfo(data.SalesInfo);
+        this.ownersInfo = buildOwnersInfo(data.OwnerInfos);
+        this.extraFeatures = buildExtraFeatures(data.ExtraFeatures);
+        this.landLines = buildLandLines(data.Landlines);
       }
 
 
     };
+
+
+    var buildLandLines = function(data){
+
+      // data not given - return an empty array.
+      if(isUndefinedOrNull(data))
+        return [];
+
+      var landLineAttr = {
+        adjustedUnitPrice:   "AdjustedUnitPrice",
+        calculatedValue:     "CalculatedValue",  
+        depth:               "Depth",            
+        frontFeet:           "FrontFeet",        
+        landUse:             "LandUse",          
+        landlineType:        "LandlineType",     
+        muniZone:            "MuniZone",         
+        percentCondition:    "PercentCondition", 
+        totalAdjustments:    "TotalAdjustments", 
+        unitType:            "UnitType",         
+        units:               "Units",            
+        useCode:             "UseCode",          
+        zone:                "Zone"              
+      };                  
+
+      var landLines = [];
+
+      _.each(data, function(originalLandLine){
+        var landLine = {};            
+        _.each(landLineAttr, function(value, key){
+            landLine[key] = originalLandLine[value]; 
+        });
+        landLines.push(landLine);
+      });
+
+      return landLines;
+            
+    };
+
+    var buildExtraFeatures = function(data){
+
+      // data not given - return an empty array.
+      if(isUndefinedOrNull(data))
+        return [];
+
+      var extraFeatureAttr = {
+        actualYearBuilt:   "ActualYearBuilt",   
+        adjustedUnitPrice: "AdjustedUnitPrice", 
+        depreciatedValue:  "DepreciatedValue",  
+        description:       "Description",       
+        units:             "Units",             
+        useCode:           "UseCode"            
+      };                  
+
+      var extraFeatures = [];
+
+      _.each(data, function(originalExtraFeature){
+        var extraFeature = {};            
+        _.each(extraFeatureAttr, function(value, key){
+            extraFeature[key] = originalExtraFeature[value]; 
+        });
+        extraFeatures.push(extraFeature);
+      });
+
+      return extraFeatures;
+            
+    };
+
+    var buildOwnersInfo = function(data){
+
+      // data not given - return an empty array.
+      if(isUndefinedOrNull(data))
+        return [];
+
+      var ownerInfoAttr = {
+        description:     "Description",  
+        marriedFlag:     "MarriedFlag",  
+        name:            "Name",         
+        percentageOwn:   "PercentageOwn",
+        role:            "Role",         
+        tenancyCd:       "TenancyCd"    
+      };                  
+
+      var ownersInfo = [];
+
+      _.each(data, function(originalOwnerInfo){
+        var ownerInfo = {};            
+        _.each(ownerInfoAttr, function(value, key){
+            ownerInfo[key] = originalOwnerInfo[value]; 
+        });
+        ownersInfo.push(ownerInfo);
+      });
+
+      return ownersInfo;
+            
+    };
+
+    var buildSalesInfo = function(data){
+
+      // data not given - return an empty array.
+      if(isUndefinedOrNull(data))
+        return [];
+
+      var saleInfoAttr = {
+        appraiserNote:        "AppraiserNote",            
+        dateOfSale:           "DateOfSale",             
+        docuementStamps:      "DocuementStamps",        
+        etlRunId:             "Etlrun_id",              
+        grantee2:             "Grantee2",               
+        granteeName:          "GranteeName",            
+        grantor2:             "Grantor2",               
+        grantorName:          "GrantorName",            
+        lineNumber:           "LineNumber",             
+        modifTp:              "Modif_tp",               
+        note:                 "Note",                   
+        officialRecordBook:   "OfficialRecordBook",     
+        officialRecordPage:   "OfficialRecordPage",     
+        qualifiedFlag:        "QualifiedFlag",          
+        qualifiedSourceCode:  "QualifiedSourceCode",    
+        reasonCode:           "ReasonCode",             
+        receivedDate:         "ReceivedDate",           
+        resetFlag:            "ResetFlag",              
+        reviewCoad:           "ReviewCoad",             
+        salePrice:            "SalePrice",              
+        salesId:              "SalesId",                
+        salesInstrument:      "SalesInstrument",        
+        strap:                "Strap",                  
+        vacantFlag:           "VacantFlag",             
+        validCoad:            "ValidCoad",              
+        verifyCoad:           "VerifyCoad"              
+      };                  
+
+      var salesInfo = [];
+
+      _.each(data, function(originalSaleInfo){
+        var saleInfo = {};            
+        _.each(saleInfoAttr, function(value, key){
+            saleInfo[key] = originalSaleInfo[value]; 
+        });
+        salesInfo.push(saleInfo);
+      });
+
+      return salesInfo;
+            
+    };
+
 
     /**
      *
      **/
     var buildSiteAddresses = function(data){
 
-      // data - return an empty array.
+      // data not given - return an empty array.
       if(isUndefinedOrNull(data))
         return [];
 
@@ -51,7 +204,8 @@ angular.module('propertySearchApp')
                              streetSuffix:              "StreetSuffix",         
                              streetSuffixDirection:     "StreetSuffixDirection",
                              unit:                      "Unit",                 
-                             zip:                       "Zip"}                  
+                             zip:                       "Zip"
+                            };                  
 
       var siteAddresses = [];
 
--- test/spec/services/propertyService.js
+++ test/spec/services/propertyService.js
@@ -144,4 +144,200 @@ describe('Service: propertyService', function () {
 
   });
 
+  // test salesInfo
+  it('SalesInfoData not exists maps to salesInfo empty array', function(){
+    var property = new propertyService.Property({SalesInfo:[]});
+    expect(property.salesInfo).toEqual([]);
+  });
+
+  it('SalesInfo null maps to salesInfo empty array', function(){
+    var property = new propertyService.Property({SalesInfo:null});
+    expect(property.salesInfo).toEqual([]);
+  });
+
+  it('map incoming SalesInfo fields to our salesInfo model fields',function(){
+    var givenProperty = {SalesInfo: [{
+      AppraiserNote: null,
+      DateOfSale: "9/1/1982",
+      DocuementStamps: 0,
+      Etlrun_id: 0,
+      Grantee2: null,
+      GranteeName: null,
+      Grantor2: null,
+      GrantorName: null,
+      LineNumber: 0,
+      Modif_tp: null,
+      Note: null,
+      OfficialRecordBook: "11557",
+      OfficialRecordPage: "0429",
+      QualifiedFlag: null,
+      QualifiedSourceCode: null,
+      ReasonCode: "00",
+      ReceivedDate: null,
+      ResetFlag: "",
+      ReviewCoad: null,
+      SalePrice: 60000,
+      SalesId: 3,
+      SalesInstrument: null,
+      Strap: null,
+      VacantFlag: "",
+      ValidCoad: null,
+      VerifyCoad: null
+    }]};
+
+    var expectedProperty = {salesInfo: [{
+      appraiserNote: null,
+      dateOfSale: "9/1/1982",
+      docuementStamps: 0,
+      etlRunId: 0,
+      grantee2: null,
+      granteeName: null,
+      grantor2: null,
+      grantorName: null,
+      lineNumber: 0,
+      modifTp: null,
+      note: null,
+      officialRecordBook: "11557",
+      officialRecordPage: "0429",
+      qualifiedFlag: null,
+      qualifiedSourceCode: null,
+      reasonCode: "00",
+      receivedDate: null,
+      resetFlag: "",
+      reviewCoad: null,
+      salePrice: 60000,
+      salesId: 3,
+      salesInstrument: null,
+      strap: null,
+      vacantFlag: "",
+      validCoad: null,
+      verifyCoad: null
+    }]};
+
+    var property = new propertyService.Property(givenProperty);
+    expect(property.salesInfo).toEqual(expectedProperty.salesInfo);
+
+  });
+
+
+  // test ownersInfo.
+  it('OwnerInfosData not exists maps to ownersInfo empty array', function(){
+    var property = new propertyService.Property({OwnerInfos:[]});
+    expect(property.ownersInfo).toEqual([]);
+  });
+
+  it('OwnerInfosData null maps to ownersInfo empty array', function(){
+    var property = new propertyService.Property({OwnerInfos:null});
+    expect(property.ownersInfo).toEqual([]);
+  });
+
+  it('map incoming OwnersInfo fields to our ownersInfo model fields',function(){
+    var givenProperty = {OwnerInfos: [{
+      Description: "",
+      MarriedFlag: "",
+      Name: "Joan McTemp",
+      PercentageOwn: 0,
+      Role: null,
+      TenancyCd: ""
+    }]};
+
+    var expectedProperty = {ownersInfo: [{
+      description: "",
+      marriedFlag: "",
+      name: "Joan McTemp",
+      percentageOwn: 0,
+      role: null,
+      tenancyCd: ""
+    }]};
+
+    var property = new propertyService.Property(givenProperty);
+    expect(property.ownersInfo).toEqual(expectedProperty.ownersInfo);
+
+  });
+
+  // test extraFeatures
+  it('ExtraFeaturesData not exists maps to ExtraFeatures empty array', function(){
+    var property = new propertyService.Property({ExtraFeatures:[]});
+    expect(property.extraFeatures).toEqual([]);
+  });
+
+  it('ExtraFeatures null maps to extraFeatures empty array', function(){
+    var property = new propertyService.Property({ExtraFeatures:null});
+    expect(property.extraFeatures).toEqual([]);
+  });
+
+  it('map incoming ExtraFeatures fields to our extraFeatures model fields',function(){
+    var givenProperty = {ExtraFeatures: [{
+      ActualYearBuilt: 1994,
+      AdjustedUnitPrice: 8,
+      DepreciatedValue: 672,
+      Description: "Chain-link Fence 4-5 ft high",
+      Units: 100,
+      UseCode: "0034 "
+    }]};
+
+    var expectedProperty = {extraFeatures: [{
+      actualYearBuilt: 1994,
+      adjustedUnitPrice: 8,
+      depreciatedValue: 672,
+      description: "Chain-link Fence 4-5 ft high",
+      units: 100,
+      useCode: "0034 "
+    }]};
+
+    var property = new propertyService.Property(givenProperty);
+    expect(property.extraFeatures).toEqual(expectedProperty.extraFeatures);
+
+  });
+
+  // test landLines
+  it('LandlinesData not exists maps to landLines empty array', function(){
+    var property = new propertyService.Property({Landlines:[]});
+    expect(property.landLines).toEqual([]);
+  });
+
+  it('Landlines null maps to landLines empty array', function(){
+    var property = new propertyService.Property({Landlines:null});
+    expect(property.landLines).toEqual([]);
+  });
+
+  it('map incoming Landlines fields to our landLines model fields',function(){
+    var givenProperty = {Landlines: [{
+      AdjustedUnitPrice: "278.1540",
+      CalculatedValue: "27815",
+      Depth: 0,
+      FrontFeet: 0,
+      LandUse: "GENERAL",
+      LandlineType: "C",
+      MuniZone: "T3 O",
+      PercentCondition: 1,
+      TotalAdjustments: 1.0302,
+      UnitType: "F ",
+      Units: "100.0000",
+      UseCode: "00 ",
+      Zone: "5700"
+    }]};
+
+    var expectedProperty = {landLines: [{
+      adjustedUnitPrice: "278.1540",
+      calculatedValue: "27815",
+      depth: 0,
+      frontFeet: 0,
+      landUse: "GENERAL",
+      landlineType: "C",
+      muniZone: "T3 O",
+      percentCondition: 1,
+      totalAdjustments: 1.0302,
+      unitType: "F ",
+      units: "100.0000",
+      useCode: "00 ",
+      zone: "5700"
+    }]};
+
+    var property = new propertyService.Property(givenProperty);
+    expect(property.landLines).toEqual(expectedProperty.landLines);
+
+  });
+
+
 })
