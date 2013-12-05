885de72c341c28d17dc161dae3f9250b46b31834
Phani Upadrasta <phanikiranur@gmail.com>
2013-12-05 10:41:08 -0500

    Show all owners in the property info box

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,14 +18,14 @@ angular.module('propertySearchApp')
       $scope.property = null;
       $scope.candidatesList = null;
     };
-	
+/*	
 	$scope.showFullListOfOwners = function() {
 		if($scope.property != null && $scope.property.ownersInfo.length > 2)
 			return true;
 		else
 			return false;
 	};
-	
+*/	
 	$scope.showFullListOfSiteAddresses = function() {
 		if($scope.property != null && $scope.property.siteAddresses.length > 1)
 			return true;
